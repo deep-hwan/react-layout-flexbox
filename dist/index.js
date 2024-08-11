f7e66ae220959719c3f68135f3869e8023446252
deep-hwan <deep@deepcomu.com>
2024-08-11 16:04:01 +0900

    0.0.1

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ViewTheme = exports.P = exports.V = void 0;
+var view_1 = require("./themes/view");
+Object.defineProperty(exports, "ViewTheme", { enumerable: true, get: function () { return view_1.ViewTheme; } });
+var P_1 = require("./widgets/P");
+Object.defineProperty(exports, "P", { enumerable: true, get: function () { return P_1.P; } });
+var V_1 = require("./widgets/V");
+Object.defineProperty(exports, "V", { enumerable: true, get: function () { return V_1.V; } });
---
+++ dist/widgets/V.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.V = void 0;
+/** @jsxImportSource @emotion/react */
+var react_1 = require("react");
+var Column_1 = __importDefault(require("./view/Column"));
+var Row_1 = __importDefault(require("./view/Row"));
+function V(_a) {
+    var children = _a.children;
+    var child = react_1.Children.only(children);
+    return (0, react_1.cloneElement)(child);
+}
+exports.V = V;
+V.Column = Column_1.default;
+V.Row = Row_1.default;
